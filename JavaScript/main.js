8e3b58db39bc267d8ed9946ca7ac3896312b19dd
HadHub <zword92@gmail.com>
2015-05-07 01:08:05 +0200

    Update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,3 +3,14 @@ $( document ).ready(function() {
 $('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
 });
 } )( jQuery );
+
+
+ 
+ 
+(function($){  
+  setInterval(function(){  
+    $("#slideshow ul li:first-child").animate({"margin-left": -900}, 1500, function(){  
+        $(this).css("margin-left",0).appendTo("#slideshow ul");  
+    });  
+  }, 4500);   
+})(jQuery);
