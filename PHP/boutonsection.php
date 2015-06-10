a7e873cf6881c7cb3897693d384b04ce6200f6ae
HadHub <zword92@gmail.com>
2015-06-11 00:06:01 +0200

    lol

--- PHP/boutonsection.php
+++ PHP/boutonsection.php
@@ -4,18 +4,17 @@
 	<!--liste des choix de base qui s'affiche que l'on soit connecté ou non-->
 	<li><a href="#"><img src="../images/lbc_logo.png" width="100" height="100"></a></li>
 	<li><a href="Accueil.php"><p id="logo">Leboncoing<span>.com</span></p></a></li>
-	</br>
-	<li class="maj"><a href="recherche.php">Faire une recherche</a></li>
-	<li class="maj"><a href="listeannonces.php">Liste des annonces</a></li>
-	<li class="maj"><a href="creerannonce.php">Déposer une annonce</a></li>
-	<li class="maj"><a href="listemembres.php">Liste des membres</a></li>
+	<li class="maj_hover"><a href="recherche.php">Faire une recherche</a></li>
+	<li class="maj_hover"><a href="listeannonces.php">Liste des annonces</a></li>
+	<li class="maj_hover"><a href="creerannonce.php">Déposer une annonce</a></li>
+	<li class="maj_hover"><a href="listemembres.php">Liste des membres</a></li>
 
 <?php 
 		//Liste des choix pour les non connectés
 	if (isset($_SESSION['adresse_mail'])=='')		
 	{
 		//Liste des choix pour les non connectés
-		echo'<li class="maj"><a href="Inscription.php"> S\'inscrire</a></li>';
+		echo'<li class="maj_hover"><a href="Inscription.php"> S\'inscrire</a></li>';
 
 	}?>
 </ul>
